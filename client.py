b5239ab4ba379572d0a0fa29cc3ddae0546f8ef0
MaximShaybakov <momon_boton@mail.ru>
2022-11-06 02:53:08 +0500

    404

--- client.py
+++ client.py
@@ -1,11 +1,10 @@
 import requests
 
-# print()
-data = requests.get('http://127.0.0.1:5000/user/1')
-# , json={'name': 'Boris',
-#                                                           'password': 'klhg#YFJH*%^J865',
-#                                                           'email': 'boris@mail.ru',
-#                                                           }, timeout=5, )
+print()
+data = requests.post('http://127.0.0.1:5000/user/', json={'name': 'Stepan',
+                                                        'password': 'klhg#YFJH*%^J865',
+                                                        'email': 'stepan@mail.ru',
+                                                        })
 
 print(data.status_code)
 print(data.text)
--- flask_server/main.py
+++ flask_server/main.py
@@ -4,4 +4,4 @@ from flask import Flask
 app = Flask('app')
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', 5000, debug=False)
+    app.run('127.0.0.1', 5000, debug=True)
