9a99abb0258deeb6c7ab84b2e5ff4a404a2bcc6a
MaximShaybakov <momon_boton@mail.ru>
2022-11-06 04:34:36 +0500

    not connect to database

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import requests
 
 
-# data = requests.post('http://127.0.0.1:5000/user/', json={'name': 'Stepan',
-#                                                            'password': 'king#YFJH*%^J865',
-#                                                            'email': 'stepan@mail.ru',
-#                                                           })
-data = requests.get('http://127.0.0.1:5000/user/1')
+data = requests.post('http://127.0.0.1:5000/user/', json={'name': 'Stepan',
+                                                           'password': 'king#YFJH*%^J865',
+                                                           'email': 'stepan@mail.ru',
+                                                          })
+# data = requests.get('http://127.0.0.1:5000/user/1')
 
 
 print(data.status_code)
